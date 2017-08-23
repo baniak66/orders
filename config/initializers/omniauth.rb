2cd79ad89757001edcc6a50168404c4e405cdc22
Pawel Licznar <pawellicznar@gmail.com>
2017-08-23 21:37:01 +0200

    Show user profile picture

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-    scope: 'public_profile', info_fields: 'id,name,link'
+    scope: 'public_profile', image_size: {width: 20, height: 20}, info_fields: 'id,name'
 end
